6ec9de1fc2bf8975630e048d5a9f95dace139def
Jman <jordylee264@gmail.com>
2021-11-12 11:22:25 +0000

    Spawning Stamps

---
+++ Assets/Scripts/CreateStamp.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateStamp : MonoBehaviour
+{
+    public GameObject[] backingStamps;
+    public GameObject[] imageStamps;
+    public GameObject stamp;
+    Vector2 spawnPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPoint.x = 0;
+        spawnPoint.y = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector2 newSpawn;
+            newSpawn.x = spawnPoint.x + 2;
+            newSpawn.y = spawnPoint.y;
+            spawnPoint = newSpawn;
+            SpawnStamp();
+        }
+    }
+
+    void SpawnStamp()
+    {
+        var newStamp = Instantiate(stamp, spawnPoint, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/GenerateStamp.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateStamp : MonoBehaviour
+{
+    CreateStamp mommyStamp;
+    GameObject back;
+    GameObject front;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mommyStamp = FindObjectOfType<CreateStamp>();
+        GenerateImages();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerateImages()
+    {
+        int randy1 = Random.Range(0, 10);
+        int randy2 = Random.Range(0, 10);
+
+        back = Instantiate(mommyStamp.backingStamps[randy1], transform.position, Quaternion.identity);
+        back.transform.parent = gameObject.transform;
+
+        front = Instantiate(mommyStamp.imageStamps[randy2], transform.position, Quaternion.identity);
+        front.transform.parent = gameObject.transform;
+    }
+}
